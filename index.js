2127e594746fbea28666dccbefdefeadcf21a8fb
Vaishnav <vshnvtrivedi@gmail.com>
2024-08-31 03:23:52 +0530

    using compression and morgan

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-
+const compression = require('compression');
+const morgan = require('morgan');
 const recipeRoutes = require('./routes/recipeRoutes');
 const userRoutes = require('./routes/userRoutes'); 
 
-
+app.use(compression());
+app.use(morgan('tiny'));
 app.use(express.json()); 
 
 
